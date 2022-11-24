ecfd829b40dc28da40dbc435d099c68dce50113d
lucasanand9 <lucasanand9@gmail.com>
2022-11-24 17:57:17 -0300

    tela de pesquisa

---
+++ src/components/perfil/perfilPesquisa.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './perfil.scss';
+
+export default function perfilPesquisa(){
+    return(
+        <div className='div-perfil-pesquisa'>
+            <div className='foto-perfil-pesquisa'></div>
+            <div className="nome-perfil-pesquisa"><h3>Lucas</h3></div>
+        </div>
+    );
+}
